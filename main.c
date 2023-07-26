b0e64014556ccbfc215ccb5947f7ea1f53b09959
samahrajabi <samahrajabi.sr@gmail.com>
2023-07-26 15:40:37 +0300

    reads data from file, stores it to database and prints it to terminal

--- main.c
+++ main.c
@@ -44,14 +44,14 @@ struct Course {
 struct Student {
     char firstName[MAX_NAME_LEN];
     char lastName[MAX_NAME_LEN];
-    int phone_number;
+    int phoneNumber;
     int level;
     int class;
     struct Course courses[MAX_COURSES]; // Array of size 20 for courses
     struct Student* nextStudent; // Linked list pointer
 };
 
-struct Student* createStudent(const char* firstName, const char* lastName, int level, int class, const int* grades) {
+struct Student* createStudent(const char* firstName, const char* lastName, int phoneNumber, int level, int class, const int* grades) {
     struct Student* newStudent = (struct Student*)malloc(sizeof(struct Student));
     strncpy(newStudent->firstName, firstName, sizeof(newStudent->firstName) - 1);
     newStudent->firstName[sizeof(newStudent->firstName) - 1] = '\0'; // Ensure null-terminated
@@ -60,6 +60,7 @@ struct Student* createStudent(const char* firstName, const char* lastName, int l
 
     newStudent->level = level;
     newStudent->class = class;
+    newStudent->phoneNumber = phoneNumber;
 
     for (int i = 0; i < MAX_COURSES; i++) {
         strncpy(newStudent->courses[i].courseName, courseNames[i], sizeof(newStudent->courses[i].courseName) - 1);
@@ -97,14 +98,38 @@ struct Course* createCourse(const char* name) {
     // Copy the name into the courseName field
     strncpy(newCourse->courseName, name, sizeof(newCourse->courseName) - 1);
     newCourse->courseName[sizeof(newCourse->courseName) - 1] = '\0'; // Ensure null-terminated
-    newCourse->nextCourse = NULL;
     return newCourse;
 }
+
+// Function to read the students' data from the file and create the student structures
+void readStudentsFromFile(FILE* file, struct Level* level) {
+    char firstName[MAX_NAME_LEN];
+    char lastName[MAX_NAME_LEN];
+    int phoneNumber;
+    int levelNumber;
+    int classNumber;
+    int grades[MAX_COURSES];
+
+    for (int i = 0; i < MAX_CLASSES; i++) {
+        for (int j = 0; j < MAX_LEVELS; j++) {
+            fscanf(file, "%s %s %d %d %d", firstName, lastName, &phoneNumber, &levelNumber, &classNumber);
+            for (int k = 0; k < MAX_COURSES; k++) {
+                fscanf(file, "%d", &grades[k]);
+            }
+            struct Student* newStudent = createStudent(firstName, lastName, phoneNumber, levelNumber, classNumber, grades);
+            // Add the new student to the linked list of students in the current class
+            newStudent->nextStudent = level->classes[i].studentsHead;
+            level->classes[i].studentsHead = newStudent;
+        }
+    }
+}
+
+
 
 
 int main() {
     // ... main function code ...
-
+    /*
     // Creating courses and linking them to a student
     struct Course* courses[MAX_COURSES];
     for (int i = 0; i < MAX_COURSES; i++) {
@@ -113,6 +138,50 @@ int main() {
     }
 
     // ... rest of main function code ...
+    */
+    // Open the file for reading
+    FILE* file = fopen("students_with_class.txt", "r");
+    if (file == NULL) {
+        printf("Error opening the file.\n");
+        return 1;
+    }
 
+    // Creating courses
+    struct Course* courses[MAX_COURSES];
+    for (int i = 0; i < MAX_COURSES; i++) {
+        courses[i] = createCourse(courseNames[i]);
+    }
+
+    // Create the level structure
+    struct Level level;
+    // Initialize the studentsHead to NULL for all classes
+    for (int i = 0; i < MAX_CLASSES; i++) {
+        level.classes[i].studentsHead = NULL;
+    }
+
+    // Read students' data from the file and create the student structures
+    readStudentsFromFile(file, &level);
+
+    // Close the file after reading
+    fclose(file);
+
+    // Printing the data of the first 10 students to the terminal
+    for (int i = 0; i < MAX_CLASSES; i++) {
+        printf("Class %d:\n", i + 1);
+        struct Student* currentStudent = level.classes[i].studentsHead;
+        int count = 0;
+        while (currentStudent && count < 10) {
+            printf("Name: %s %s\n", currentStudent->firstName, currentStudent->lastName);
+            printf("Phone Number: %d\n", currentStudent->phoneNumber);
+            printf("Level: %d\n", currentStudent->level);
+            printf("Class: %d\n", currentStudent->class);
+            for (int j = 0; j < MAX_COURSES; j++) {
+                printf("%s Grade: %d\n", currentStudent->courses[j].courseName, currentStudent->courses[j].grade);
+            }
+            printf("\n");
+            currentStudent = currentStudent->nextStudent;
+            count++;
+        }
+    }
     return 0;
 }
