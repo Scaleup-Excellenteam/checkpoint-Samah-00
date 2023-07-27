7b67e986c6baa4e6858572a396107eec2aae4e9e
samah-00 <samahra@edu.hac.ac.il>
2023-07-27 13:43:21 +0300

    added encryption and decryption functions.

--- main.c
+++ main.c
@@ -33,7 +33,7 @@ const char* courseNames[] = {
     "Course 20"
 };
 
-
+// structures and functions
 // Structure for Course
 struct Course {
     char courseName[MAX_NAME_LEN];
@@ -296,6 +296,55 @@ void printMenu() {
     printf("Enter your choice: ");
 }
 
+
+
+// Encryption and decryption functions
+// Function to encrypt a character using a shift cipher
+char encryptChar(char ch, int shift) {
+    if (('A' <= ch && ch <= 'Z') || ('a' <= ch && ch <= 'z')) {
+        if ('A' <= ch && ch <= 'Z') {
+            ch = ((ch - 'A' + shift) % 26) + 'A';
+        } else {
+            ch = ((ch - 'a' + shift) % 26) + 'a';
+        }
+    }
+    return ch;
+}
+
+
+// Function to encrypt the file using a shift cipher
+void encryptFile(FILE* inputFile, FILE* outputFile, int shift) {
+    char ch;
+    while ((ch = fgetc(inputFile)) != EOF) {
+        ch = encryptChar(ch, shift);
+        fputc(ch, outputFile);
+    }
+}
+
+
+// Function to decrypt a character using a shift cipher
+char decryptChar(char ch, int shift) {
+    if (('A' <= ch && ch <= 'Z') || ('a' <= ch && ch <= 'z')) {
+        if ('A' <= ch && ch <= 'Z') {
+            ch = ((ch - 'A' + (26 - shift)) % 26) + 'A';
+        } else {
+            ch = ((ch - 'a' + (26 - shift)) % 26) + 'a';
+        }
+    }
+    return ch;
+}
+
+
+// Function to decrypt the file using a shift cipher
+void decryptFile(FILE* inputFile, FILE* outputFile, int shift) {
+    char ch;
+    while ((ch = fgetc(inputFile)) != EOF) {
+        ch = decryptChar(ch, shift);
+        fputc(ch, outputFile);
+    }
+}
+
+/*
 int main() {
     // ... main function code ...
 
@@ -326,6 +375,9 @@ int main() {
     // Close the file after reading
     fclose(file);
 
+
+
+    /*
     // Main menu loop
     int choice;
     do {
@@ -377,7 +429,7 @@ int main() {
                 printf("Invalid choice. Please try again.\n");
         }
     } while (choice != 6);
-
+    
     // Free memory for all students
     for (int i = 0; i < MAX_LEVELS; i++) {
         for (int j = 0; j < MAX_CLASSES; j++) {
@@ -392,3 +444,68 @@ int main() {
 
     return 0;
 }
+*/
+
+int main() {
+    // ... main function code ...
+
+
+    // Open the file for reading
+    FILE* file = fopen("students_with_class.txt", "r");
+    if (file == NULL) {
+        printf("Error opening the file.\n");
+        return 1;
+    }
+
+
+    // Create the encrypted binary file
+    FILE* encryptedFile = fopen("encrypted_students.bin", "wb");
+    if (encryptedFile == NULL) {
+        printf("Error creating the encrypted file.\n");
+        fclose(file);
+        return 1;
+    }
+
+
+    // Encrypt the file and save the encrypted content to the binary file
+    int shift = 3; // You can change the shift value as needed
+    encryptFile(file, encryptedFile, shift);
+
+
+    // Close the files
+    fclose(file);
+    fclose(encryptedFile);
+
+
+    // Open the encrypted binary file for reading
+    encryptedFile = fopen("encrypted_students.bin", "rb");
+    if (encryptedFile == NULL) {
+        printf("Error opening the encrypted file.\n");
+        return 1;
+    }
+
+
+    // Create the decrypted file
+    FILE* decryptedFile = fopen("decrypted_students.txt", "w");
+    if (decryptedFile == NULL) {
+        printf("Error creating the decrypted file.\n");
+        fclose(encryptedFile);
+        return 1;
+    }
+
+
+    // Decrypt the file and save the decrypted content to the text file
+    decryptFile(encryptedFile, decryptedFile, shift);
+
+
+    // Close the files
+    fclose(encryptedFile);
+    fclose(decryptedFile);
+
+
+    return 0;
+}
+
+
+
+
